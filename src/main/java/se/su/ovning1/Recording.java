00f2600976a7871d865ec28607028bfecd86b238
juliaehaack <julia.ekenrydhaack@icloud.com>
2025-04-08 11:36:08 +0200

    Lade till alla klasser.

---
+++ src/main/java/se/su/ovning1/Recording.java
@@ -0,0 +1,46 @@
+package se.su.ovning1;
+
+public abstract class Recording extends Item implements PriceableWithVAT25{
+
+    private final String artist;
+    private final int year;
+    private int condition;
+    private final double price;
+
+    protected Recording(String name, String artist, int year, int condition, double price){
+        super (name);
+        this.artist = artist;
+        this.year = year;
+        this.condition = condition;
+        this.price = price;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public abstract String getType();
+
+    public int getCondition(){
+        return condition;
+    }
+
+    @Override
+    public double getPrice(){
+        double newPrice = condition * price / 10;
+        return (newPrice <= 10) ? 10 : newPrice;
+    }
+
+    @Override
+    public String toString(){
+        return "String";
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    protected double getOriginalPrice(){
+        return price;
+    }
+}
